1d31184d12cae07c689b6c9690f7942767c5d0c3
albertoflorence <alberto.sabatier@gmail.com>
2023-10-28 16:21:25 -0300

    fix: matches test to use token

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -1,11 +1,13 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import { modelStub, modelStubBulk } from './helper';
 import MatchModel from '../database/models/MatchModel';
+import UserModel from '../database/models/UserModel';
 
 chai.use(chaiHttp);
 const { expect, request } = chai;
@@ -31,6 +33,8 @@ const dataNotInProgress = {
 describe('matches', () => {
   beforeEach(() => {
     sinon.restore();
+    sinon.stub(jwt, 'verify').returns({ id: 1 } as any);
+    modelStub(UserModel, 'findByPk', { id: 1 });
   });
 
   describe('GET /matches', () => {
@@ -53,7 +57,9 @@ describe('matches', () => {
     it('should return only matches with inProgress = false', async () => {
       const data = [dataNotInProgress];
       modelStubBulk(MatchModel, 'findAll', data);
-      const result = await request(app).get('/matches?inProgress=false');
+      const result = await request(app)
+        .get('/matches?inProgress=false')
+        .set('Authorization', 'Bearer any_token');
       expect(result).to.have.status(200);
       expect(result.body).to.deep.equal(data);
     });
@@ -62,7 +68,9 @@ describe('matches', () => {
   describe('PATCH matches/:id/finish', () => {
     it('should return 200 if match finished', async () => {
       modelStub(MatchModel, 'findByPk', dataInProgress);
-      const result = await request(app).patch('/matches/1/finish');
+      const result = await request(app)
+        .patch('/matches/1/finish')
+        .set('Authorization', 'Bearer any_token');
       expect(result).to.have.status(200);
       expect(result.body).to.deep.equal({ message: 'Match finished' });
     });
@@ -71,14 +79,18 @@ describe('matches', () => {
   describe('PATCH matches/:id', () => {
     it('should return 404 if match not found', async () => {
       modelStub(MatchModel, 'findByPk', null);
-      const result = await request(app).patch('/matches/1');
+      const result = await request(app)
+        .patch('/matches/1')
+        .set('Authorization', 'Bearer any_token');
       expect(result).to.have.status(404);
       expect(result.body).to.deep.equal({ message: 'Match not found' });
     });
 
     it('should return 401 if match already finished', async () => {
       modelStub(MatchModel, 'findByPk', dataNotInProgress);
-      const result = await request(app).patch('/matches/1');
+      const result = await request(app)
+        .patch('/matches/1')
+        .set('Authorization', 'Bearer any_token');
       expect(result).to.have.status(401);
       expect(result.body).to.deep.equal({ message: 'Match already finished' });
     });
@@ -87,7 +99,8 @@ describe('matches', () => {
       modelStub(MatchModel, 'findByPk', dataInProgress);
       const result = await request(app)
         .patch('/matches/1/finish')
-        .send({ homeTeamGoals: 3, awayTeamGoals: 1 });
+        .send({ homeTeamGoals: 3, awayTeamGoals: 1 })
+        .set('Authorization', 'Bearer any_token');
       expect(result).to.have.status(200);
     });
   });
